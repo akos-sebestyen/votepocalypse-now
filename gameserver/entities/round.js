7cc624bc5c89c2cd72989dc45c44ddb9b2e6039b
Akos Sebestyen <akos.sebestyen@mediavalet.com>
2017-09-02 13:47:58 -0700

    allow question to be set

--- gameserver/entities/round.js
+++ gameserver/entities/round.js
@@ -4,8 +4,12 @@ class Round extends BaseEntity {
 
     constructor(onRoundUpdate, startDelay){
         const defaultState = {
+            askingPlayerId: null,
             roundStartDate: null,
-            hasStarted: false
+            hasStarted: false,
+            question: null,
+            option1: null,
+            option2: null
         };
 
         super(defaultState, onRoundUpdate);
@@ -13,7 +17,7 @@ class Round extends BaseEntity {
         this._startDelay = startDelay || 5000;
     }
 
-    startRound(cb) {
+    startRound(askingPlayerId, cb) {
         setTimeout(() => {
             this.applyEvent({type: Round.EventType.START_ROUND});
             if(cb) cb();
@@ -22,18 +26,34 @@ class Round extends BaseEntity {
         this.applyEvent({
             type: Round.EventType.REQUEST_START_ROUND,
             payload: {
+                askingPlayerId,
                 roundStartDate: new Date(new Date().getTime() + this._startDelay)
             }
         });
     }
 
+    setQuestion(question, option1, option2) {
+        this.applyEvent({
+            type: Round.EventType.SET_QUESTION,
+            payload: {question, option1, option2}
+        })
+    }
 
     _reducer(state, {type, payload}){
         switch(type){
             case Round.EventType.REQUEST_START_ROUND:
-                return Object.assign({}, state, {roundStartDate: payload.roundStartDate});
+                return Object.assign({}, state, {
+                    askingPlayerId: payload.askingPlayerId,
+                    roundStartDate: payload.roundStartDate
+                });
             case Round.EventType.START_ROUND:
                 return Object.assign({}, state, {hasStarted: true});
+            case Round.EventType.SET_QUESTION:
+                return Object.assign({}, state, {
+                    question: payload.question,
+                    option1: payload.option1,
+                    option2: payload.option2
+                });
             default: return Object.assign({}, state);
         }
     }
@@ -41,7 +61,8 @@ class Round extends BaseEntity {
 
 Round.EventType = {
     REQUEST_START_ROUND: "REQUEST_START_ROUND",
-    START_ROUND: "START_ROUND"
+    START_ROUND: "START_ROUND",
+    SET_QUESTION: "SET_QUESTION"
 };
 
 module.exports = Round;
--- gameserver/test/round.spec.js
+++ gameserver/test/round.spec.js
@@ -24,14 +24,16 @@ describe('Round', function() {
         expect(round.state).to.eql(round._defaultState);
     });
 
-    it('start a round', (cb) => {
+    const startRound =  (cb) => {
+        const askingPlayerId = "player1";
         const onAction = ({type, state}) => {
-            if(type === Round.EventType.REQUEST_START_ROUND){
+            if (type === Round.EventType.REQUEST_START_ROUND) {
                 expect(state.roundStartDate).to.exist;
                 expect(state.hasStarted).to.eql(false);
+                expect(state.askingPlayerId).to.eql(askingPlayerId);
                 expect(round.eventHistory).to.have.length(1);
             }
-            if(type === Round.EventType.START_ROUND){
+            if (type === Round.EventType.START_ROUND) {
                 expect(state.hasStarted).to.eql(true);
                 expect(round.eventHistory).to.have.length(2);
             }
@@ -39,11 +41,28 @@ describe('Round', function() {
 
         const spy = sinon.spy(onAction);
 
-        round = new Round(spy, 50);
+        round = new Round(spy, 5);
 
-        round.startRound(() => {
+        round.startRound(askingPlayerId, () => {
             expect(spy).to.have.been.calledTwice;
             cb();
         });
+    };
+
+    it('start a round', (cb) => {
+        startRound(cb);
+    });
+
+    it('set a question for the round', (cb) => {
+        startRound(()=>{
+            const question = "Would you rather eat a burger or eat a hot dog";
+            const option1 = "eat a burger";
+            const option2 = "eat a hotdog";
+            round.setQuestion(question, option1, option2);
+            expect(round.state.question).to.eql(question);
+            expect(round.state.option1).to.eql(option1);
+            expect(round.state.option2).to.eql(option2);
+            cb();
+        })
     });
 });
